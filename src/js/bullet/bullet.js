134ae6156c4b141a9964b2f04e7c317bff1cc5db
hongfz <hongfz16@163.com>
2018-07-10 22:58:15 +0800

    add constant to every class:

--- src/js/bullet/bullet.js
+++ src/js/bullet/bullet.js
@@ -1,20 +1,21 @@
 import Sprite from '../base/sprite';
-import bulletStyle from '../constant/constant'
-import bulletLife from '../constant/constant'
-import gameCor from '../constant/constant'
+// import bulletStyle from '../constant/constant'
+// import bulletLife from '../constant/constant'
+// import gameCor from '../constant/constant'
 
 export default class Bullet extends Sprite {
-  constructor(x = 0, y = 0, r = 0, velx = 0, vely = 0) {
+  constructor(constant, x = 0, y = 0, r = 0, velx = 0, vely = 0) {
     super(x, y, r);
     this.setVelocity(velx, vely);
-    this.life = bulletLife;
+    this.constant = constant;
+    this.life = this.constant.bulletLife;
   }
 
   update() {
     this.circle.center.x += this.vel.x;
     this.circle.center.y += this.vel.y;
-    this.circle.center.x %= gameCor.width;
-    this.circle.center.y %= gameCor.height;
+    this.circle.center.x %= this.constant.gameCor.width;
+    this.circle.center.y %= this.constant.gameCor.height;
     if(this.life > 0) {
       this.life -= 1;
     }
@@ -23,8 +24,8 @@ export default class Bullet extends Sprite {
   drawtoCanvas(ctx) {
     ctx.beginPath();
     ctx.arc(this.getX(), this.getY(), this.getRadius(), 0, 2 * Math.PI);
-    ctx.strokeStyle = bulletStyle.strokeColor;
-    ctx.lineWidth = bulletStyle.strokeSize;
+    ctx.strokeStyle = this.constant.bulletStyle.strokeColor;
+    ctx.lineWidth = this.constant.bulletStyle.strokeSize;
     ctx.stroke();
   }
 }
