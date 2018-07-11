e8eb095b3c0fa08c58c61dc417dd37adfbb03763
ycdfwzy <ycdfwzy@outlook.com>
2018-07-11 20:18:02 +0800

    delete demo

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
-import './js_backup/libs/weapp-adapter'
-import './js_backup/libs/symbol'
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
 
 // import Main from './js_backup/main'
 import Main from './js/main';
