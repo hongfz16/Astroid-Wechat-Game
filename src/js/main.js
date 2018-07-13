66d022ad407a05add2e0842c21f47d377d947a12
hongfz <hongfz16@163.com>
2018-07-13 22:27:53 +0800

    before merge

--- src/js/main.js
+++ src/js/main.js
@@ -22,25 +22,25 @@ export default class Main {
     //     console.log(res.openIdList);
     //   }
     // });
-    let button = wx.createUserInfoButton({
-      type: 'text',
-      text: '获取用户信息',
-      style: {
-        left: 10,
-        top: 76,
-        width: 200,
-        height: 40,
-        lineHeight: 40,
-        backgroundColor: '#ff0000',
-        color: '#ffffff',
-        textAlign: 'center',
-        fontSize: 16,
-        borderRadius: 4
-      }
-    })
-    button.onTap((res) => {
-      console.log(res)
-    })
+    // let button = wx.createUserInfoButton({
+    //   type: 'text',
+    //   text: '获取用户信息',
+    //   style: {
+    //     left: 10,
+    //     top: 76,
+    //     width: 200,
+    //     height: 40,
+    //     lineHeight: 40,
+    //     backgroundColor: '#ff0000',
+    //     color: '#ffffff',
+    //     textAlign: 'center',
+    //     fontSize: 16,
+    //     borderRadius: 4
+    //   }
+    // })
+    // button.onTap((res) => {
+    //   console.log(res)
+    // })
 
     this.aniId = 0;
     // wx.setPreferredFramesPerSecond(fps);
